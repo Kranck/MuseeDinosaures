7d9db19fb7c8d52db95609367254ab7ae4726dcb
Tchoalien <florianbayle94@gmail.com>
2016-11-25 15:55:45 +0100

    Add auto configure button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,45 @@
 $( document ).ready(function()
 {
-    function OpenDiv(target) {
+
+    SetButton( $("button[name='button']") );
+
+    //Configure un bouton pour ouvrir une div qu'il cible avec son attribut target
+    function SetButton(targetButton)
+    {
+      var divName = $(targetButton).data("target"); //Name de la div cible
+
+      var TargetDiv = $( "div[name='"+divName+"']");  //Cible
+
+
+      $(targetButton).on('click', function functionName() {
+
+        if(isOpened(TargetDiv))
+          CloseDiv(TargetDiv);
+        else
+          OpenDiv(TargetDiv);
+
+      });
+    }
+
+    //Affiche un element target
+    function OpenDiv(target)
+    {
       $(target).fadeIn("slow");
     }
-    function CloseDiv(target) {
+
+    //Rend invisible un element target
+    function CloseDiv(target)
+    {
       $(target).fadeOut("slow");
     }
+
+    //Verifie si l'element target est visible
+    function isOpened(target)
+    {
+      if($(target).is(':visible'))
+        return true;
+
+      return false;
+    }
+
 });
