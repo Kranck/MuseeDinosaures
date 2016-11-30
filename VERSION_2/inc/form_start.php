7fe87540b71e57e5148de4b39c6fdd6a2e6d1e20
Yannis <frizzy.rastay@gmail.com>
2016-11-30 14:18:43 +0100

    Deplacement des images de drapeaux

--- VERSION_2/inc/form_start.php
+++ VERSION_2/inc/form_start.php
@@ -4,13 +4,13 @@
             <input id="pseudo_val" type="text" name="field1" placeholder="Pseudo">
 
             <button type="button" name="button" id="lang_button" onclick="ChangeLanguage('French')">
-                <img src="../flags/french.png" id="lang_button_image">
+                <img src="ui/french.png" id="lang_button_image">
             </button>
             <button type="button" name="button" id="lang_button" onclick="ChangeLanguage('English')">
-                <img src="../flags/english.png" id="lang_button_image">
+                <img src="ui/english.png" id="lang_button_image">
             </button>
             <button type="button" name="button" id="lang_button" onclick="ChangeLanguage('Spanish')">
-                <img src="../flags/spain.png" id="lang_button_image">
+                <img src="ui/spain.png" id="lang_button_image">
             </button>
         </fieldset>
 
